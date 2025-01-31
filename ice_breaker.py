eccf33d266282ba52ac6215c8b32d91884d360f5
cal-dortiz <djortiz@berkeley.edu>
2025-01-30 18:58:19 -0600

    Add output parser and front end

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,14 +1,15 @@
 from langchain_core.prompts import PromptTemplate
 # from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
-from langchain_core. output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parser import summary_parser, Summary
 from dotenv import load_dotenv
+from typing import Tuple
 
 load_dotenv()
 
-def ice_break_with(name: str) -> str:
+def ice_break_with(name: str) -> Tuple[Summary, str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
@@ -16,20 +17,23 @@ def ice_break_with(name: str) -> str:
         Given the linkedin information {information} about a personf from I want you to create:
         1. a short summary
         2. two interestng facts about them
+
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()}  # used to load the pydantic object schema and paste it in the format instructions in the prompt
     )
 
     # llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     llm = ChatOllama(model="llama3.2")
 
-    chain = summary_prompt_template | llm 
+    chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
 
 if __name__ == "__main__":
     print("Ice Breaker Enter")
